dbd81841853ad719377cc783665cf2355ad2361f
Ben Luck <benjamonstyles@protonmail.com>
2025-09-22 19:57:22 +0200

    added the player object and drew it to screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import * 
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -9,22 +10,27 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
-
         clock = pygame.time.Clock()
         dt = 0
-
 
         # end loop when window closed
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        # Clear the screen
         screen.fill('black')
+
+        # Draw the player
+        player.draw(screen)
+        
+        # 'flip' the display ?
         pygame.display.flip()
 
-
+        # Cap the framerate at 60fps and get the delta time.
         dt = clock.tick(60) / 1000  # Amount of seconds between each loop.
 
 
