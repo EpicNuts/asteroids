475027cc83006bd43c147d60f9c74838d823833b
Ben Luck <benjamonstyles@protonmail.com>
2025-09-22 21:35:55 +0200

    added asteroid objects and gave them movement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import * 
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting Asteroids!")
@@ -10,8 +12,21 @@ def main():
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = updatable, drawable
+
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = updatable, drawable, asteroids
+
+    AsteroidField.containers = updatable
+
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
+    
     clock = pygame.time.Clock()
+
 
     while True:
         dt = clock.tick(60) / 1000  # Amount of seconds between each loop.
@@ -25,11 +40,12 @@ def main():
         screen.fill('black')
 
         # update the player
-        player.update(dt)
+        updatable.update(dt)
         
         # render the player
-        player.draw(screen)
-        
+        for sprite in drawable:
+            sprite.draw(screen)
+
         # 'flip' the display ?
         pygame.display.flip()
 
